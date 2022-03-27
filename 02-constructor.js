861f3559cbc283a52d858dd9c7c9c9f198102849
adikharadkar <adityakharadkar27@gmail.com>
2022-03-27 14:44:55 +0530

    Update 02-constructor.js

--- 02-constructor.js
+++ 02-constructor.js
@@ -2,14 +2,22 @@ class Friends {
   name;
   age;
 
+  // Constructor Method
   constructor(name, age) {
     this.name = name;
     this.age = age;
   }
 }
 
+// Create & Initialize Object 1
 const f1 = new Friends("Joey", 29);
+
+// Create & Initialize Object 2
 const f2 = new Friends("Changler", 28);
+
+// Print Class "Friends"
 console.log(Friends);
+
+// Print Objects f1 & f2
 console.log(f1);
 console.log(f2);
