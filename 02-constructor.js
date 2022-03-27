4fa0061f6950b648e90ff9512adf45c4311989c1
adikharadkar <adityakharadkar27@gmail.com>
2022-03-27 14:38:29 +0530

    Create 02-constructor.js

---
+++ 02-constructor.js
@@ -0,0 +1,15 @@
+class Friends {
+  name;
+  age;
+
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const f1 = new Friends("Joey", 29);
+const f2 = new Friends("Changler", 28);
+console.log(Friends);
+console.log(f1);
+console.log(f2);
